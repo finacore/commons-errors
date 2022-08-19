bf9b2e34f963d5dfd8eaddf267562d1eb92df108
Denys G. Santos <gsdenys@gmail.com>
2022-08-19 15:55:37 -0400

    add validation error code

---
+++ errors/validationerror.go
@@ -0,0 +1,13 @@
+package errors
+
+type ValidationError struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
+}
+
+func Validation(field string, message string) *ValidationError {
+	return &ValidationError{
+		Field:   field,
+		Message: message,
+	}
+}
