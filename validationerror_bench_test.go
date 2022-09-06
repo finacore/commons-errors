17115f29a311bde5c813c6e6260fa69a30f07e31
Denys G. Santos <gsdenys@gmail.com>
2022-09-06 16:31:15 -0400

    organize benchmark

--- validationerror_bench_test.go
+++ validationerror_bench_test.go
@@ -1,36 +1,11 @@
 package commonserrors
 
 import (
-	"reflect"
 	"testing"
 )
 
-func BenchmarkCreateValidationError(b *testing.B) {
-	type args struct {
-		field   string
-		message string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *ValidationError
-	}{
-		{
-			name: "with-data",
-			args: args{field: "test", message: "just some test"},
-			want: &ValidationError{Field: "test", Message: "just some test"},
-		},
-		{
-			name: "without-data",
-			args: args{},
-			want: &ValidationError{Field: "", Message: ""},
-		},
-	}
-	for _, tt := range tests {
-		b.Run(tt.name, func(b *testing.B) {
-			if got := CreateValidationError(tt.args.field, tt.args.message); !reflect.DeepEqual(got, tt.want) {
-				b.Errorf("CreateValidationError() = %v, want %v", got, tt.want)
-			}
-		})
+func Benchmark_CreateValidationError(b *testing.B) {
+	if got := CreateValidationError("some-field", "somme error"); got == nil {
+		b.Errorf("the object should not be nil")
 	}
 }
