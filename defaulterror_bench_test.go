9c5b78e34041c2f942207e182253de2fea7ee4ba
Denys G. Santos <gsdenys@gmail.com>
2022-09-06 16:17:49 -0400

    organize benchmark

--- defaulterror_bench_test.go
+++ defaulterror_bench_test.go
@@ -1,65 +1,24 @@
 package commonserrors
 
 import (
-	"errors"
-	"reflect"
+	"fmt"
 	"testing"
 )
 
 func BenchmarkCreateDefaultError(b *testing.B) {
-	type args struct {
-		message string
-	}
-	tests := []struct {
-		name string
-		args args
-		want *DefaultError
-	}{
-		{
-			name: "with-message",
-			args: args{message: "just some test"},
-			want: &DefaultError{Message: "just some test"},
-		},
-		{
-			name: "without-message",
-			args: args{message: ""},
-			want: &DefaultError{Message: ""},
-		},
-	}
-	for _, tt := range tests {
-		b.Run(tt.name, func(b *testing.B) {
-			if got := CreateDefaultError(tt.args.message); !reflect.DeepEqual(got, tt.want) {
-				b.Errorf("New() = %v, want %v", got, tt.want)
-			}
-		})
+	if got := CreateDefaultError("this is an error message"); got == nil {
+		b.Errorf("unable to create default error")
 	}
 }
 
 func BenchmarkMakeDefaultError(b *testing.B) {
-	type args struct {
-		err error
+	if got := MakeDefaultError(fmt.Errorf("this is an error message")); got == nil {
+		b.Errorf("unable to create default error")
 	}
-	tests := []struct {
-		name string
-		args args
-		want *DefaultError
-	}{
-		{
-			name: "with-message",
-			args: args{err: errors.New("just some test")},
-			want: &DefaultError{Message: "just some test"},
-		},
-		{
-			name: "without-message",
-			args: args{err: errors.New("")},
-			want: &DefaultError{Message: ""},
-		},
-	}
-	for _, tt := range tests {
-		b.Run(tt.name, func(b *testing.B) {
-			if got := MakeDefaultError(tt.args.err); !reflect.DeepEqual(got, tt.want) {
-				b.Errorf("MakeDefaultError() = %v, want %v", got, tt.want)
-			}
-		})
+}
+
+func BenchmarkStatus(b *testing.B) {
+	if got := MakeDefaultError(fmt.Errorf("this is an error message")).Status(500); got == nil {
+		b.Errorf("unable to create default error")
 	}
 }
