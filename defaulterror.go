4ff20bb6d8be44482b898bce697596a4432b34e2
Denys G. Santos <gsdenys@gmail.com>
2022-10-27 23:19:10 -0400

    organizing documentation

--- defaulterror.go
+++ defaulterror.go
@@ -1,38 +1,39 @@
-// Package cerr encapsulates some methods and data structure responsibles to organize
-// and manage the erros obtained duruing the program execution.
+// Package cerr encapsulates some methods and data structure responsible to
+// organize and manage the erros obtained during the program execution.
 //
-// Among the characteristics, the one that stand out are the capability to convert the data
-// structure to an string, following the error interface implemented by the golang error, As
-// well the agregation of an integer error code that simplify the error recognitions less the
-// necessity of make reflections in the received object.
+// Among the characteristics, the one that stand out are the capability to convert
+// the data structure to an string, following the error interface implemented by
+// the golang error, As well the aggregation of an integer error code that
+// simplify the error recognitions less the necessity of make reflections in the
+// received object.
 //
-// The first step to use this package is to get it through the command below, then you will be
-// able to import it in your code.
+// The first step to use this package is to get it through the command below, then
+// you will be able to import it in your code.
 //
 //	go get github.com/gsdenys/cerr
 //
-// To avoid the conflicts between packages during the imnport phase, is strongly recommended
-// select other name for this package in the moment of import like shown below:
+// Then import it as shown below:
 //
 //	import (
 //		 "github.com/gsdenys/cerr"
 //	)
 //
-// Once imported you can use the package as you prefer. So read the function and methods
-// documentation to know how to use this packagage.
+// Once imported you can use the package as you prefer. So read the function and
+// methods documentation to know how to use this package.
 package cerr
 
-// DefaultError is a representaton of the most basic kind of error in this package. It's
-// composed by a message description and an error code. By definition, the error code is
-// not rendered when this structure is coverted to JSON.
+// DefaultError is a representation of the most basic kind of error in this
+// package. It's composed by a message description and an error code. By
+// definition, the error code is not rendered when this structure is covered to
+// JSON.
 type DefaultError struct {
 	Message string `json:"message"`
 	Code    int    `json:"-"`
 }
 
-// CreateDefaultError returns a DefaultError object that contains the error message and the
-// error code. By default the error code associated to this error is 500, but it's can be
-// changed calling the method Status(int)
+// CreateDefaultError returns a DefaultError object that contains the error
+// message and the error code. By default the error code associated to this error
+// is 500, but it's can be changed calling the method Status(int)
 //
 // Usage:
 //
@@ -45,12 +46,12 @@ func CreateDefaultError(message string) *DefaultError {
 	return err.Status(500)
 }
 
-// MakeDefaultError returns a DefaultError object that contains the error message and the
-// error code. This method uses a preexistent error  in order to capture their error message
-// and calls the CreateDefaultError method.
+// MakeDefaultError returns a DefaultError object that contains the error message
+// and the error code. This method uses a preexistent error  in order to capture
+// their error message and calls the CreateDefaultError method.
 //
-// As the CreateDefaultError method, this one also set the default status as 500,  but it's
-// can be changed calling the method Status(int)
+// As the CreateDefaultError method, this one also set the default status as 500,
+// but it's can be changed calling the method Status(int)
 //
 // Usage:
 //
@@ -59,8 +60,8 @@ func MakeDefaultError(err error) *DefaultError {
 	return CreateDefaultError(err.Error())
 }
 
-// Error method that returns a string literal containing the error message associated to the
-// data structure.
+// Error method that returns a string literal containing the error message associated
+// to the data structure.
 //
 // Usage:
 //
@@ -70,8 +71,8 @@ func (e *DefaultError) Error() string {
 	return e.Message
 }
 
-// Status method is responsable to overwrite the default error code that has set to the data
-// structure and returns the own changed data structure.
+// Status method is responsable to overwrite the default error code that has set to the
+// data structure and returns the own changed data structure.
 //
 // Usage:
 //
--- validationerror.go
+++ validationerror.go
@@ -1,43 +1,22 @@
-// Package cerr encapsulates some methods and data structure responsibles to organize
-// and manage the erros obtained duruing the program execution.
-//
-// Among the characteristics, the one that stand out are the capability to convert the data
-// structure to an string, following the error interface implemented by the golang error, As
-// well the agregation of an integer error code that simplify the error recognitions less the
-// necessity of make reflections in the received object.
-//
-// The first step to use this package is to get it through the command below, then you will be
-// able to import it in your code.
-//
-//	go get github.com/gsdenys/cerr
-//
-// To avoid the conflicts between packages during the imnport phase, is strongly recommended
-// select other name for this package in the moment of import like shown below:
-//
-//	import (
-//		"github.com/gsdenys/cerr"
-//	)
-//
-// Once imported you can use the package as you prefer. So read the function and methods
-// documentation to know how to use this packagage.
 package cerr
 
 import "fmt"
 
-// ValidationError is a representaton of the error that coming from the validation library like
-// go-playground/validator. It's composed by the DefaultError fields inheritance and the field
-// name
+// ValidationError is a representation of the error that coming from the
+// validation library like go-playground/validator. It's composed by the
+// DefaultError fields inheritance and the field name
 type ValidationError struct {
 	Field string `json:"field"`
 	DefaultError
 }
 
-// CreateValidationError function to build a new ValidatonError object based on the name of field
-// and the error messge passed by paramenter.
+// CreateValidationError function to build a new ValidationError object based on
+// the name of field and the error message passed by parameter.
 
-// CreateValidationError returns a CreateValidationError object that contains the field name, the
-// error message and the error code. By default the error code associated to this error is 400,
-// but it's can be changed calling the method Status(int)
+// CreateValidationError returns a CreateValidationError object that contains the
+// field name, the error message and the error code. By default the error code
+// associated to this error is 400, but it's can be changed calling the method
+// Status(int)
 //
 // Usage:
 //
@@ -52,8 +31,8 @@ func CreateValidationError(field string, message string) *ValidationError {
 	return val
 }
 
-// Error method that returns a string literal containing the composition of the field name and the
-// message separated by ': '
+// Error method that returns a string literal containing the composition of the
+// field name and the message separated by ':'
 //
 // e.g. name: the name should not be empty
 //
@@ -65,12 +44,13 @@ func (e *ValidationError) Error() string {
 	return fmt.Sprintf("%s: %s", e.Field, e.Message)
 }
 
-// Status method is responsable to overwrite the default error code that has set to the data
-// structure and returns the own changed data structure.
+// Status method is responsable to overwrite the default error code that has set
+// to the data structure and returns the own changed data structure.
 //
 // Usage:
 //
-//	err := cerr.CreateValidationError("name", "the name should not be empty").Status(200)
+//	err := cerr.CreateValidationError("name", "the name should not be empty")
+//	err.Status(200)
 func (e *ValidationError) Status(status int) *ValidationError {
 	e.Code = status
 	return e
